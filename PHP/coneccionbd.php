31dafee418a84e58944c70b098bbe6cc1e67b585
Alvin Alonso <79954801+Alonso-Nunez@users.noreply.github.com>
2023-01-03 21:52:05 -0600

    Delete PHP directory

--- PHP/coneccionbd.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$conexion = mysqli_connect("localhost:3306", "root", "", "pro_is");
-if (!$conexion) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "error: " . mysqli_connect_errno() . PHP_EOL;
-    exit;
-}
-?>
