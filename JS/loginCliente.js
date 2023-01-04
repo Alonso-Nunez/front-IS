feacdc8bfa799198a138bbdb81227f55c1bea80e
Alonso Nunez <alonsonunezvaz@gmail.com>
2023-01-04 00:35:52 -0600

    corregir

--- JS/loginCliente.js
+++ JS/loginCliente.js
@@ -9,7 +9,6 @@ var formulario__Registrarse = document.querySelector (".formulario_Registrarse")
 var caja__fondoLogin = document.querySelector (".caja_fondoLogin");
 var caja__fondoRegistro = document.querySelector (".caja_fondoRegistro");
 var formClienteReg = document.getElementById("formclienteregist");
-var formNegocioReg = document.getElementById("formnegocioregist");
 
 formClienteReg.addEventListener('submit', function(e){
     e.preventDefault();
@@ -47,41 +46,6 @@ formClienteReg.addEventListener('submit', function(e){
     .finally(() => {
         window.location.href = 'loginCliente.html';
     })
-})
-
-formNegocioReg.addEventListener('submit', function(e){
-    e.preventDefault();
-    console.log("click")
-    var datos =  new FormData(formNegocioReg);
-    fetch('http://127.0.0.1:8000/api/negocio', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-             "nombre": datos.get('nombre'),
-             "direccion": datos.get('direccion'),
-             "telefono": datos.get('telefono'),
-             "correo": datos.get('correo'),
-             "horario": datos.get('horario'),
-             "password": datos.get('password')})
-        })
-        .then(response => response.json())
-        .then((response) => {
-            console.log(response)
-            console.log(response.data)
-            if (response.success) {
-                window.alert(response.messages[0]);
-            }
-            else {
-                window.alert(response.messages[0]);
-            }
-        })
-        .finally(() => {
-            windows.location.href = 'loginNegocio.html';
-        })
-
 })
 
 function anchoPagina(){
