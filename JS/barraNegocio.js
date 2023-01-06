0abb5b32142de4edb725877040e1caaeb4715727
Mayer Abraham Pérez González <38333546+MayerPerez@users.noreply.github.com>
2023-01-06 11:30:01 -0600

    update info negocio

--- JS/barraNegocio.js
+++ JS/barraNegocio.js
@@ -3,6 +3,7 @@ var tiposesion = window.sessionStorage.getItem('tipo_sesion');
 var logout = document.getElementById('logout');
 
 var formDatosNeg = document.getElementById("formularioDatosN");
+var formEditNeg = document.getElementById("formularioEditNeg");
 
 if (formDatosNeg != null) {
 
@@ -32,10 +33,66 @@ if (formDatosNeg != null) {
             window.alert(response.messages[0]);
         }
     })
-    
 }
 
+if (formEditNeg != null) {
+    //Servicio que devuelve información del negocio
+    fetch('http://127.0.0.1:8000/api/negocio/auth', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '+ verToken
+        }
+    })
+    .then(response => response.json())
+    .then((response) => {
+        if(response.success){
+            formEditNeg.nombre.value = response.data.nombre;
+            formEditNeg.telefono.value = response.data.telefono;
+            formEditNeg.direccion.value = response.data.direccion;
+            formEditNeg.horario.value = response.data.horario;
+            formEditNeg.correo.value = response.data.correo;
+        }
+        else{
+            window.alert(response.messages[0]);
+        }
+    })
 
+    //Servicio que actualiza la información del negocio
+    formEditNeg.addEventListener('submit', function(e){
+        e.preventDefault();
+        console.log("click")
+    
+        var datos =  new FormData(formEditNeg);
+        fetch('http://127.0.0.1:8000/api/negocio', {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ verToken
+            },
+            body: JSON.stringify({
+                "nombre": datos.get('nombre'),
+                "direccion": datos.get('direccion'),
+                "telefono": datos.get('telefono'),
+                "correo": datos.get('correo'),
+                "horario": datos.get('horario'),
+                "password": datos.get('password')})
+        })
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response)
+            console.log(response.data)
+            if(response.success){
+                window.alert(response.messages[0]);
+            }
+            else{
+                window.alert(response.messages[0]);
+            }
+        })
+    })
+}
 
 if (verToken == null && tiposesion == null) {
     window.location.href = 'inicioSesion.html';
