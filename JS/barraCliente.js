cedc9254d8c68ce253f5cd98c1f1a9a78a519afd
Mayer Abraham Pérez González <38333546+MayerPerez@users.noreply.github.com>
2023-01-12 15:52:24 -0600

    load publicaciones cliente

--- JS/barraCliente.js
+++ JS/barraCliente.js
@@ -5,6 +5,9 @@ var logout = document.getElementById('logout');
 var formDatosCliente = document.getElementById("formularioDatosC");
 var formEditarCliente = document.getElementById("formularioEditC");
 
+var divListaProdNeg = document.getElementById("lista-productos");
+console.log(divListaProdNeg)
+
 if (verToken == null && tiposesion == null) {
     window.location.href = 'inicioSesion.html';
 }else if(verToken != null && tiposesion == "negocio"){
@@ -101,6 +104,111 @@ if (formEditarCliente != null) {
         })
     })
 }
+
+if (divListaProdNeg != null) {
+    var datos = null;
+    fetch("http://127.0.0.1:8000/api/publicacion", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + verToken,
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        //console.log(response)
+        //console.log(response.data)
+        if (response.success) {
+          datos = response.data;
+          const host = "http://127.0.0.1:8000";
+          //La siguiente variable cambia el numero de productos por fila
+          var numElem = 2;
+  
+          var div = document.createElement("div");
+          div.classList.add("card-deck", "mb-3", "text-center");
+          divListaProdNeg.appendChild(div);
+          for (let index = 0; index < datos.length; index++) {
+            div.innerHTML += `<div class="card mb-4 shadow-sm">
+                      <div class="card-header">
+                          <h4 class="my-0 font-weight-bold">${
+                            datos[index].titulo
+                          }</h4>
+                      </div>
+                      <div class="card-body">
+                          <img src="${
+                            host + datos[index].pathImage
+                          }" class="card-img-top">
+                          <h1 class="card-title pricing-card-title precio"><span class="">${
+                            datos[index].precio
+                          }</span></h1>
+  
+                          <ul class="list-unstyled mt-3 mb-4">
+                              <li></li>
+                              <li>Negocio: <b>${datos[index].negocio}</b></li>
+                              <li>${datos[index].nombre}</li>
+                              <li>${datos[index].descripcion}</li>
+                              <li>Cantidad Disponible: ${
+                                datos[index].disponibilidad
+                              } </li>
+                              <li>Promocion: ${datos[index].promocion}</li>
+                          </ul>
+                          <input type="submit" class="btn btn-success" value="Mas información" data-id="${
+                            datos[index].id
+                          }">
+                          <input type="submit" class="btn btn-danger" value="Reservar" data-id="${
+                            datos[index].id
+                          }">
+                      </div>
+                  </div>`;
+            //Agrega otro div (fila) para los productos
+            if (index != 0 && index % numElem == 0) {
+              //console.log(index)
+              div = document.createElement("div");
+              div.classList.add("card-deck", "mb-3", "text-center");
+              divListaProdNeg.appendChild(div);
+            }
+          }
+  
+          /*var btnsDelete = document.getElementsByClassName("btn-danger");
+          for (d of btnsDelete) {
+            d.addEventListener("click", function () {
+              var id = this.getAttribute("data-id");
+              fetch("http://127.0.0.1:8000/api/publicacion/" + id, {
+                method: "DELETE",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                  Authorization: "Bearer " + verToken,
+                },
+              })
+                .then((response) => response.json())
+                .then((response) => {
+                  //console.log(response)
+                  if (response.success) {
+                    window.alert(response.messages[0]);
+                    location.reload();
+                  } else {
+                    window.alert(response.messages[0]);
+                  }
+                });
+            });
+          }
+  
+          var btnsEdit = document.getElementsByClassName("btn-success");
+          for (e of btnsEdit) {
+            e.addEventListener("click", function () {
+              var idP = this.getAttribute("data-id");
+              window.sessionStorage.setItem("publicacion_id", idP);
+              window.location.href = "editarOfertaNegocio.html";
+            });
+          }*/
+        } else {
+          window.alert(response.messages[0]);
+        }
+      });
+  }
+  
 
 
 addEventListener('DOMContentLoaded', () =>{
